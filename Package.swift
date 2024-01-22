56c8662753f73f4593a9137246df323d4840305f
OhKanghoon <ggaa96@naver.com>
2024-01-22 17:51:26 +0900

    initial commit
    Co-authored-by: jaxtynSong <jaxtyn@daangn.com>

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "KarrotListKit",
+  platforms: [.iOS(.v15)],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "KarrotListKit",
+      targets: ["KarrotListKit"]
+    ),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "KarrotListKit"
+    ),
+    .testTarget(
+      name: "KarrotListKitTests",
+      dependencies: ["KarrotListKit"]
+    ),
+  ]
+)
