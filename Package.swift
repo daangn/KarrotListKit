e430fd810fd787e39f3b404e557371a9f4f44cbc
OhKanghoon <ggaa96@naver.com>
2024-01-23 23:10:23 +0900

    chore: update minimum iOS versions

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "KarrotListKit",
-  platforms: [.iOS(.v15)],
+  platforms: [.iOS(.v13)],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
