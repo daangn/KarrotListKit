01ac700340f70f4a57d73f185f21b6408fd2077c
OhKanghoon <ggaa96@naver.com>
2024-03-10 16:44:32 +0900

    style: Package.swift 불필요 주석 제거

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
   name: "KarrotListKit",
   platforms: [.iOS(.v13)],
   products: [
-    // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
       name: "KarrotListKit",
       targets: ["KarrotListKit"]
@@ -23,13 +22,11 @@ let package = Package(
       .upToNextMajor(from: "3.0.0")
     ),
     .package(
-      url: "https://github.com/apple/swift-docc-plugin",
+      url: "https://github.com/apple/swift-docc-plugin.git",
       from: "1.3.0"
     ),
   ],
   targets: [
-    // Targets are the basic building blocks of a package, defining a module or a test suite.
-    // Targets can depend on other targets in this package and products from dependencies.
     .target(
       name: "KarrotListKit",
       dependencies: [
