462f0286868a19101b6c4a3fb68786a1842c7cfd
Jaxtyn <Jaxtyn@daangn.com>
2024-04-02 19:12:24 +0900

    MBP-4002 Component & AnyComponent 테스트 코드를 추가해요.

---
+++ Tests/KarrotListKitTests/AnyComponentTests.swift
@@ -0,0 +1,58 @@
+//
+//  AnyComponentTests.swift
+//
+//
+//  Created by Jaxtyn on 2024/04/02.
+//
+
+import XCTest
+
+@testable import KarrotListKit
+
+final class AnyComponentTests: XCTestCase {
+
+  func test_properties() {
+    // given
+    let component = DummyComponent()
+    let anyComponent = AnyComponent(component)
+
+    // when & then
+    XCTAssertEqual(component.layoutMode, anyComponent.layoutMode)
+    XCTAssertEqual(component.reuseIdentifier, anyComponent.reuseIdentifier)
+  }
+
+  func test_renderContent() {
+    // given
+    let content = UIView()
+    var component = ComponentStub()
+    component.contentStub = content
+    let anyComponent = AnyComponent(component)
+
+    // when
+    let renderContent = anyComponent.renderContent(coordinator: ())
+
+    // then
+    XCTAssertIdentical(renderContent, content)
+  }
+
+  func test_render() {
+    // given
+    let component = ComponentSpy()
+    let anyComponent = AnyComponent(component)
+
+    // when
+    anyComponent.render(in: .init(), coordinator: ())
+
+    // then
+    XCTAssertEqual(component.renderCallCount, 1)
+  }
+
+  func test_type() {
+    // given
+    let component = ComponentSpy()
+    let anyComponent = AnyComponent(component)
+
+    // when & then
+    XCTAssertIdentical(anyComponent.as(ComponentSpy.self), component)
+  }
+}
