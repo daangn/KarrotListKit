52516d2f5f61f2ab154e675d8c88f02cd3d7ec3e
Jaxtyn <Jaxtyn@daangn.com>
2024-04-02 19:11:19 +0900

    MBP-4002 Component 관련 파일에 주석을 추가해요.

--- Sources/KarrotListKit/Component/AnyComponent.swift
+++ Sources/KarrotListKit/Component/AnyComponent.swift
@@ -4,62 +4,108 @@
 
 import UIKit
 
+/// A type-erased wrapper for any `Component`'s `ViewModel` that conforms to `Equatable`.
 public struct AnyViewModel: Equatable {
 
   private let base: any Equatable
 
+  /// Creates a new instance of `AnyViewModel` by wrapping the given `Component`'s `ViewModel`.
+  ///
+  /// - Parameter base: The `Component` whose `ViewModel` to wrap.
   init(_ base: some Component) {
     self.base = base.viewModel
   }
 
+  /// Returns a Boolean value indicating whether two `AnyViewModel` instances are equal.
+  ///
+  /// - Parameters:
+  ///   - lhs: The left-hand side value to compare.
+  ///   - rhs: The right-hand side value to compare.
+  /// - Returns: `true` if `lhs` and `rhs` are equal; otherwise, `false`.
   public static func == (lhs: AnyViewModel, rhs: AnyViewModel) -> Bool {
     lhs.base.isEqual(rhs.base)
   }
 }
 
+/// A type-erased wrapper for any `Component` that conforms to `Component` and `Equatable`.
 public struct AnyComponent: Component, Equatable {
   private let box: any ComponentBox
 
+  /// The any `Component` instance.
   public var base: any Component {
     box.base
   }
 
+  /// The layout mode of the component's content.
   public var layoutMode: ContentLayoutMode {
     box.layoutMode
   }
 
+  /// A reuse identifier for the component.
   public var reuseIdentifier: String {
     box.reuseIdentifier
   }
 
+  /// The `ViewModel` of the component.
   public var viewModel: AnyViewModel {
     AnyViewModel(box.base)
   }
 
+  /// Creates a new instance of `AnyComponent` by wrapping the given `Component`.
+  ///
+  /// - Parameter base: The `Component` to wrap.
   public init(_ base: some Component) {
     self.box = AnyComponentBox(base)
   }
 
+  /// Creates the content object and configures its initial state.
+  ///
+  /// - Parameter coordinator: The coordinator to use for rendering the content.
+  /// - Returns: The rendered content.
   public func renderContent(coordinator: Any) -> UIView {
     box.renderContent(coordinator: coordinator)
   }
 
+  /// Updates the state of the specified content with new information
+  ///
+  /// - Parameters:
+  ///   - content: The content to render.
+  ///   - coordinator: The coordinator to use for rendering.
   public func render(in content: UIView, coordinator: Any) {
     box.render(in: content, coordinator: coordinator)
   }
 
+  /// Lays out the specified content in the given container view.
+  ///
+  /// - Parameters:
+  ///   - content: The content to layout.
+  ///   - container: The view to layout the content in.
   public func layout(content: UIView, in container: UIView) {
     box.layout(content: content, in: container)
   }
 
+  /// Attempts to downcast the underlying `Component` to the specified type.
+  ///
+  /// - Parameter _: The type to downcast to.
+  /// - Returns: The underlying `Component` as the specified type, or `nil` if the downcast fails.
   public func `as`<T>(_: T.Type) -> T? {
     box.base as? T
   }
 
+  /// Creates the custom instance that you use to communicate changes from your view to other parts of your SwiftUI interface.
+  ///
+  /// If your view doesn’t interact with SwiftUI interface, providing a coordinator is unnecessary.
+  /// - Returns: A new `Coordinator` instance.
   public func makeCoordinator() -> Any {
     box.makeCoordinator()
   }
 
+  /// Returns a Boolean value indicating whether two `AnyComponent` instances are equal.
+  ///
+  /// - Parameters:
+  ///   - lhs: The left-hand side value to compare.
+  ///   - rhs: The right-hand side value to compare.
+  /// - Returns: `true` if `lhs` and `rhs` are equal; otherwise, `false`.
   public static func == (lhs: AnyComponent, rhs: AnyComponent) -> Bool {
     lhs.viewModel == rhs.viewModel
   }
