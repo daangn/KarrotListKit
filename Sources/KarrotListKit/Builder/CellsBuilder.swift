14003e8e4c457d735fd0bb531756307e1d8bcf13
Jaxtyn <Jaxtyn@daangn.com>
2024-04-10 23:33:18 +0900

    MBP-4280 result builder 주석 및 필요 구현을 추가해요.

--- Sources/KarrotListKit/Builder/CellsBuilder.swift
+++ Sources/KarrotListKit/Builder/CellsBuilder.swift
@@ -4,21 +4,50 @@
 
 import Foundation
 
+/// Provides a resultBuilder for creating an array of cell.
 @resultBuilder
 public enum CellsBuilder {
   public static func buildBlock(_ components: Cell...) -> [Cell] {
     components
   }
 
+  public static func buildBlock(_ components: [Cell]...) -> [Cell] {
+    components.flatMap { $0 }
+  }
+
   public static func buildBlock(_ components: [Cell]) -> [Cell] {
     components
+  }
+
+  public static func buildOptional(_ component: [Cell]?) -> [Cell] {
+    component ?? []
   }
 
   public static func buildEither(first component: [Cell]) -> [Cell] {
     component
   }
 
+  public static func buildEither(first component: Cell...) -> [Cell] {
+    component
+  }
+
+  public static func buildEither(first component: () -> [Cell]) -> [Cell] {
+    component()
+  }
+
   public static func buildEither(second component: [Cell]) -> [Cell] {
     component
+  }
+
+  public static func buildExpression(_ expression: Cell...) -> [Cell] {
+    expression
+  }
+  
+  public static func buildExpression(_ expression: [Cell]...) -> [Cell] {
+    expression.flatMap { $0 }
+  }
+
+  public static func buildArray(_ components: [[Cell]]) -> [Cell] {
+    components.flatMap { $0 }
   }
 }
