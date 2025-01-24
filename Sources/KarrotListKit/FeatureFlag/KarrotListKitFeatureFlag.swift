b4ef008cd8424bea39b3537c0c15412402df8662
Jaxtyn <Jaxtyn@daangn.com>
2025-01-24 16:18:48 +0900

    `usesCachedViewSize` 검증을 위해 FeatureFlag 기능을 지원해요.

---
+++ Sources/KarrotListKit/FeatureFlag/KarrotListKitFeatureFlag.swift
@@ -0,0 +1,15 @@
+//
+//  Copyright (c) 2025 Danggeun Market Inc.
+//
+
+import Foundation
+
+/// An interface for injecting a feature flag provider.
+public enum KarrotListKitFeatureFlag {
+
+  /// The feature flag provider used by `KarrotListKit`.
+  ///
+  /// By default, this is set to `DefaultFeatureFlagProvider`.
+  /// You can replace it with a custom provider to change the feature flag behavior.
+  public static var provider: FeatureFlagProviding = DefaultFeatureFlagProvider()
+}
