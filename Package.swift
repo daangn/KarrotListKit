0c11020cf2d27fb04c73aadbc2f5f7931c0b0d2e
Dongkyu Kim <stleamist@me.com>
2025-06-30 16:43:08 +0900

    feat: 최소 지원 버전을 iOS 16.0으로 상향

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "KarrotListKit",
-  platforms: [.iOS(.v13)],
+  platforms: [.iOS(.v16)],
   products: [
     .library(
       name: "KarrotListKit",
