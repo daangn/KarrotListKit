7d81132747bc8266f64565569b204ac6928b86b9
Dongkyu Kim <stleamist@me.com>
2025-07-02 15:12:54 +0900

    build(sample): `KarrotListKitSampleApp` 프로젝트 추가

---
+++ Examples/KarrotListKitSampleApp/KarrotListKitSampleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  Copyright © 2025 Danggeun Market Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ Examples/KarrotListKitSampleApp/KarrotListKitSampleApp/SampleApp.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2025 Danggeun Market Inc. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
