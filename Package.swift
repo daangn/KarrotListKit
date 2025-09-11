7bebafea78fd67c78e916cbc5e6b7b219387f3d4
Elon Park (엘론) <elon.park@daangn.com>
2025-09-11 14:30:18 +0900

    chore: add KarrotListKitMacros macro target

--- Package.swift
+++ Package.swift
@@ -1,11 +1,12 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import CompilerPluginSupport
 import PackageDescription
 
 let package = Package(
   name: "KarrotListKit",
-  platforms: [.iOS(.v13)],
+  platforms: [.iOS(.v13), .macOS(.v10_15)],
   products: [
     .library(
       name: "KarrotListKit",
@@ -13,6 +14,7 @@ let package = Package(
     ),
   ],
   dependencies: [
+    .package(url: "https://github.com/swiftlang/swift-syntax.git", "509.0.0"..<"602.0.0"),
     .package(
       url: "https://github.com/ra1028/DifferenceKit.git",
       .upToNextMajor(from: "1.0.0")
@@ -23,11 +25,26 @@ let package = Package(
       name: "KarrotListKit",
       dependencies: [
         "DifferenceKit",
+        "KarrotListKitMacros",
+      ]
+    ),
+    .macro(
+      name: "KarrotListKitMacros",
+      dependencies: [
+        .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+        .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
       ]
     ),
     .testTarget(
       name: "KarrotListKitTests",
       dependencies: ["KarrotListKit"]
+    ),
+    .testTarget(
+      name: "KarrotListKitMacrosTests",
+      dependencies: [
+        "KarrotListKitMacros",
+        .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+      ]
     ),
   ]
 )
